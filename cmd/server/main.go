271bdcfc791022fa22104bc7eef52fb2af5e8d7d
rushyn <tony_alexandrov@hotmail.com>
2024-10-11 16:39:43 +1000

    ch 3 l1 done

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	const conStr = "amqp://guest:guest@localhost:5672/"
+
+	newCon, err := amqp.Dial(conStr)
+	if err != nil{
+		fmt.Println("amqp Daal Error!!")
+		fmt.Printf("%s\n", err.Error())
+	}
+	defer newCon.Close()
+	fmt.Println("Peril server Started")
+	fmt.Println("----------------------------------")
+	fmt.Println("Wait for a signal (Ctrl+C) to exit")
+	fmt.Println("----------------------------------")
+
+	ch, err := newCon.Channel()
+	if err != nil{
+		fmt.Println("channel Error!!")
+		fmt.Printf("%s\n", err.Error())
+	}
+
+
+	err = pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+		IsPaused: true,
+	})
+	if err != nil {
+		fmt.Println("PublishJason Error")
+		fmt.Println(err.Error())
+	}
+
 }
---
+++ internal/pubsub/main.go
@@ -0,0 +1,32 @@
+package pubsub
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error{
+	log.Println("Publishing Message to Ch")
+	Jval, err := json.Marshal(val)
+	if err != nil{
+		fmt.Println("fail to marshal $val")
+		fmt.Println(err.Error())
+		return err
+	}
+	
+	err = ch.PublishWithContext(context.Background(), exchange, key, false, false, amqp.Publishing{
+		ContentType:     "application/json",
+		Body:            Jval,
+	})
+	if err != nil{
+		fmt.Println("fail to PublishWithContext")
+		fmt.Println(err.Error())
+		return err
+	}
+
+	return nil
+}
